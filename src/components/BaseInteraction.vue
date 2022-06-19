33f33824d789d9f4e9adeee50372f6eec0729c96
Raimund Canzler <git@rai-canzler.de>
2022-06-19 19:52:47 +0200

    first commit

---
+++ src/components/BaseInteraction.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+	title: string;
+	action: string;
+}>();
+</script>
+
+<template>
+	<div class="greetings">
+		<h2>{{ props.title }}</h2>
+        <p>{{ props.action }}</p>
+	</div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import BaseInteraction from "../components/BaseInteraction.vue";
+const interactions = [
+	{
+		title: "Test 1",
+		action: "Aktion 1",
+	},
+	{
+		title: "Test 2",
+		action: "Aktion 2",
+	},
+];
+</script>
+
+<template>
+	<main class="grid place-content-center h-full">
+		<h1 class="text-7xl text-center">DigiFunc&trade;</h1>
+		<BaseInteraction
+			v-for="interaction in interactions"
+			:title="interaction.title"
+			:action="interaction.action"
+		/>
+	</main>
+</template>
+
